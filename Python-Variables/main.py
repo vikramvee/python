63eb51db16dede6e8f0fe5edcbb107cfc70ba186
vikramvee <vikram.vee@outlook.com>
2020-04-13 06:53:25 +0530

    python-variables

---
+++ Python-Variables/main.py
@@ -0,0 +1,14 @@
+ #Variables are used to assign the data or information in the memory that you may want to keep for later. 
+ 
+song = 'Song'
+year = 2018
+artist = 'artist'
+genere = 'genere'
+duration, publisher = 10, 'publisher'
+
+print(song);
+print(year);
+print( artist);
+print(genere);
+print(duration);
+print(publisher);

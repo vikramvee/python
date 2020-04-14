738b2f67a3d6c6114c2f4a3b81ae4b088b968e9f
vikramvee <vikram.vee@outlook.com>
2020-04-14 07:26:35 +0530

    Added function

---
+++ Python-function/main.py
@@ -0,0 +1,25 @@
+song = 'Song'
+year = 2018
+artist = 'artist'
+genere = 'genere'
+length = 3.5
+duration, publisher = 10, 'publisher'
+isHitSong = True
+
+def Genre():
+    return genere
+
+def Artist():
+    return artist
+
+def Year():
+    return year
+
+def IsHitSong():
+    return isHitSong
+
+print(Genre())
+print(Artist())
+print(Year())
+print(IsHitSong())
+
